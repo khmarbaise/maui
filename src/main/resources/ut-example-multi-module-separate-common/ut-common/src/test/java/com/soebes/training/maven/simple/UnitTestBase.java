95b3113d2d6726ee887f1dd536b2968a173160f8
Karl Heinz Marbaise <info@soebes.de>
2012-06-18 20:33:13 +0200

    - Unit Test   Example with separate common module

---
+++ src/main/resources/ut-example-multi-module-separate-common/ut-common/src/test/java/com/soebes/training/maven/simple/UnitTestBase.java
@@ -0,0 +1,8 @@
+package com.soebes.training.maven.simple;
+
+public class UnitTestBase {
+
+	public static void message() {
+		System.out.println(" ---> This is the Unit Test Base class. <----");
+	}
+}
